e41ed00e3f1d23d99adf582280c6b22028ab9963
Victor Rodrigues <victor.rodrigues@dtm.local>
2026-08-26 11:07:41 -0300

    feat(MyRecipeBook.Communication): Add RegisterUserAccount DTO

---
+++ src/Shared/MyRecipeBook.Communication/Requests/UserAccount/RegisterUserAccountRequest.cs
@@ -0,0 +1,10 @@
+namespace MyRecipeBook.Communication.Requests.UserAccount;
+
+public class RegisterUserAccountRequest
+{   
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+}
