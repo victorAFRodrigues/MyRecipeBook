314201ade72cf5d9e0b40ffd47c2c8338abe2e3e
Victor Rodrigues <victor.rodrigues@dtm.local>
2026-08-26 11:40:38 -0300

    fix(MyRecipeBook.Exception): Solve bug on resource files and ajust csproj

--- src/Shared/MyRecipeBook.Exception/ResourceMessagesException.Designer.cs
+++ src/Shared/MyRecipeBook.Exception/ResourceMessagesException.Designer.cs
@@ -21,14 +21,14 @@ namespace MyRecipeBook.Exception {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ResourceMessageException {
+    internal class ResourceMessagesException {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ResourceMessageException() {
+        internal ResourceMessagesException() {
         }
         
         /// <summary>
@@ -38,7 +38,7 @@ namespace MyRecipeBook.Exception {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyRecipeBook.Exception.ResourceMessageException", typeof(ResourceMessageException).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyRecipeBook.Exception.ResourceMessagesException", typeof(ResourceMessagesException).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
