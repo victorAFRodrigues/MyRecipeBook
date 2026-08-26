fe390f30819bece21899116a46201da2a50563bf
Victor Rodrigues <victor.rodrigues@dtm.local>
2026-08-26 13:42:17 -0300

    feat(MyRecipeBook.Api): Register UserEndpoint in Program.cs

--- src/Backend/MyRecipeBook.Api/Program.cs
+++ src/Backend/MyRecipeBook.Api/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
 using MyRecipeBook.Api.Config;
+using MyRecipeBook.Api.Enpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,28 +24,12 @@ app.UseRequestLocalization(localizationOptions.Value);
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("docs");
 }
 
 // app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapUserEndpoints();
 
 app.Run();
 
