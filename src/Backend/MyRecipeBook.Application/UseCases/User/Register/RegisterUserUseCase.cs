3aa572e5d38dd70c321f793bc57f46e318a07595
victor rodrigues <victor_duu@hotmail.com>
2026-08-27 09:33:40 -0300

    refactor(MyRecipeBook.Application): extract ValidateAndThrowOnFailure as an auxiliary method in RegisterUserUseCase

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -7,13 +7,18 @@ public class RegisterUserUseCase
 {
      public void Execute(RegisterUserRequest request)
      {
+          ValidateAndThrowOnFailure(request);
+
+          // executa o resto das acoes
+     }
+     
+     private void ValidateAndThrowOnFailure(RegisterUserRequest request)
+     {
           var validator = new RegisterUserValidator();
           
           var result = validator.Validate(request);
 
           if (!result.IsValid)
                throw new ErrorOnValidationException(result.Errors.Select(x => x.ErrorMessage).ToList());
-          
-          // executa o resto das acoes
      }
 }
