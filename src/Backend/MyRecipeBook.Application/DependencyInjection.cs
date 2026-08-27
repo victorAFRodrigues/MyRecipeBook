2b1f04b3cc6d9cc10e7133b2180ce1964ab49447
victor rodrigues <victor_duu@hotmail.com>
2026-08-27 17:24:40 -0300

    feat(MyRecipeBook.Application): implement IRegisterUserUseCase and register it for DI in Program.cs

--- src/Backend/MyRecipeBook.Application/DependencyInjection.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjection.cs
@@ -1,6 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Domain.Security.PasswordHashing;
+// using MyRecipeBook.Infrastructure.Security.PasswordHashing;
+
 namespace MyRecipeBook.Application;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        
+        return services;
+    }
 }
--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,16 +1,21 @@
 using Mapster;
 using MyRecipeBook.Communication.Requests.UserAccount;
+using MyRecipeBook.Domain.Security.PasswordHashing;
 using MyRecipeBook.Exception.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
-public class RegisterUserUseCase
+public class RegisterUserUseCase(IPasswordHasher passwordHasher) : IRegisterUserUseCase
 {
+     private readonly IPasswordHasher _passwordHasher = passwordHasher;
+     
      public void Execute(RegisterUserRequest request)
      {
           ValidateAndThrowOnFailure(request);
           
           var user = request.Adapt<Domain.Entities.User>();
+          
+          user.Password = _passwordHasher.HashPassword(request.Password);
      }
      
      private void ValidateAndThrowOnFailure(RegisterUserRequest request)
