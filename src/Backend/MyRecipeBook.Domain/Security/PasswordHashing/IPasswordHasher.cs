d1f45cd75a5a62a87aa216fbe2779a5853524d7d
victor rodrigues <victor_duu@hotmail.com>
2026-08-27 10:56:11 -0300

    refactor(MyRecipeBook.Domain): Add interface IPasswordHasher

--- src/Backend/MyRecipeBook.Domain/Security/PasswordHashing/IPasswordHasher.cs
+++ src/Backend/MyRecipeBook.Domain/Security/PasswordHashing/IPasswordHasher.cs
@@ -1,6 +1,7 @@
 namespace MyRecipeBook.Domain.Security.PasswordHashing;
 
-public class IPasswordHasher
+public interface IPasswordHasher
 {
-    
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
 }
