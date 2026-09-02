1a42259fc8cd12bb240fc2548aa6c5f66e2ac8fe
victor rodrigues <victor_duu@hotmail.com>
2026-09-02 12:00:37 -0300

    fix(MyRecipeBook.Controller.Api): add stringConverter to fix extra whitespace in API payload

--- src/Backend/MyRecipeBook.Controller.Api/Converter/StringConverter.cs
+++ src/Backend/MyRecipeBook.Controller.Api/Converter/StringConverter.cs
@@ -1,6 +1,23 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
 namespace MyRecipeBook.Controller.Api.Converter;
 
-public class StringConverter
+public partial class StringConverter : JsonConverter<string>
 {
-    
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value  = reader.GetString()?.Trim();
+        
+        return value == null ? value : RemoveExtraBlankSpace().Replace(value, " ");
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value);
+    }
+
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex RemoveExtraBlankSpace();
 }
