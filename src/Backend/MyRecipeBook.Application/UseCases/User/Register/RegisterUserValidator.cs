a5cb288b8bbd17b728b1977a3630dadbfb1a2c54
victor rodrigues <victor_duu@hotmail.com>
2026-09-03 18:14:18 -0300

    fix(MyRecipeBook.Application): fix RegisterUserValidator

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -8,16 +8,24 @@ public class RegisterUserValidator : AbstractValidator<RegisterUserRequest>
 {
      public RegisterUserValidator()
      {
+          // Name validation
           RuleFor(user  => user.Name)
                .NotEmpty()
-               .WithMessage(ResourceMessagesException.NAME_IS_REQUIRED);
+               .WithMessage(ResourceMessagesException.NAME_IS_EMPTY);
           
+          // Email validation
           RuleFor(user => user.Email)
-               .NotEmpty().WithMessage(ResourceMessagesException.EMAIL_IS_REQUIRED)
-               .EmailAddress().WithMessage(ResourceMessagesException.EMAIL_IS_INVALID);     
-
+               .NotEmpty().WithMessage(ResourceMessagesException.EMAIL_IS_EMPTY);
+          When(user => !string.IsNullOrWhiteSpace(user.Email), () =>
+               RuleFor(user => user.Email)
+                    .EmailAddress().WithMessage(ResourceMessagesException.EMAIL_IS_INVALID));
+          
+          // Password validation
           RuleFor(user => user.Password)
-               .NotEmpty().WithMessage(ResourceMessagesException.PASSWORD_EMPTY)
-               .MinimumLength(6).WithMessage(ResourceMessagesException.PASSWORD_IS_SHORT);
+               .NotEmpty().WithMessage(ResourceMessagesException.PASSWORD_IS_EMPTY);
+          When(user => !string.IsNullOrWhiteSpace(user.Password), () =>
+               RuleFor(user => user.Password)
+                    .MinimumLength(7).WithMessage(ResourceMessagesException.PASSWORD_IS_SHORT));
+               
      }
 }
